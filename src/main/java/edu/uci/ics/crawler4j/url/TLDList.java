cee4066c85e110f71380251cf1d060732f8c2680
JanRavn <jan at ravn dot co dot uk>
2012-11-28 11:52:43 +0000

    Switched to the Guava Tdl parsing mechanism. The built-in Crawler4J did not correctly treat * and ! in the TDL list.

--- src/main/java/edu/uci/ics/crawler4j/url/TLDList.java
+++ src/main/java/edu/uci/ics/crawler4j/url/TLDList.java
@@ -1,35 +1,15 @@
 package edu.uci.ics.crawler4j.url;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import com.google.common.net.InternetDomainName;
 
 public class TLDList {
 
-	private static Set<String> tldSet;
-	
-	public static boolean contains(String str) {
-		return tldSet.contains(str);
-	}
-	
-	static {
-		tldSet = new HashSet<String>();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(TLDList.class.getClassLoader().getResourceAsStream("tld-names.txt")));
-		String line;
-		try {
-			while ((line = reader.readLine()) != null) {
-				line = line.trim();
-				if (line.isEmpty() || line.startsWith("//")) {
-					continue;
-				}
-				tldSet.add(line);
-			}
-			reader.close();
-		} catch (IOException e) {			
-			e.printStackTrace();
-		}
-	}
-	
+    public static boolean contains(String str) {
+        return InternetDomainName.from(str).isTopPrivateDomain();
+    }
+
+    public static String domain(String str) {
+        return InternetDomainName.from(str).topPrivateDomain().name();
+    }
+
 }
--- src/test/java/edu/uci/ics/crawler4j/tests/TLDListTest.java
+++ src/test/java/edu/uci/ics/crawler4j/tests/TLDListTest.java
@@ -1,43 +1,62 @@
 package edu.uci.ics.crawler4j.tests;
 
+import com.google.common.net.InternetDomainName;
 import edu.uci.ics.crawler4j.url.URLCanonicalizer;
 import edu.uci.ics.crawler4j.url.WebURL;
 import junit.framework.TestCase;
 
 public class TLDListTest extends TestCase {
-	
-	private WebURL webUrl = new WebURL();
-	
-	private void setUrl(String url) {
-		webUrl.setURL(URLCanonicalizer.getCanonicalURL(url));
-	}
 
-	public void testTLD() {
-		
-		setUrl("http://example.com");
-		assertEquals("example.com", webUrl.getDomain());
-		assertEquals("", webUrl.getSubDomain());
-		
-		setUrl("http://test.example.com");
-		assertEquals("example.com", webUrl.getDomain());
-		assertEquals("test", webUrl.getSubDomain());
-		
-		setUrl("http://test2.test.example.com");
-		assertEquals("example.com", webUrl.getDomain());
-		assertEquals("test2.test", webUrl.getSubDomain());
-		
-		setUrl("http://test3.test2.test.example.com");
-		assertEquals("example.com", webUrl.getDomain());
-		assertEquals("test3.test2.test", webUrl.getSubDomain());
-		
-		setUrl("http://www.example.ac.jp");
-		assertEquals("example.ac.jp", webUrl.getDomain());
-		assertEquals("www", webUrl.getSubDomain());
-		
-		setUrl("http://example.ac.jp");
-		assertEquals("example.ac.jp", webUrl.getDomain());
-		assertEquals("", webUrl.getSubDomain());
-		
-	}
-	
+    private WebURL webUrl = new WebURL();
+
+    private void setUrl(String url) {
+        webUrl.setURL(URLCanonicalizer.getCanonicalURL(url));
+    }
+
+    public void testTLD() {
+
+        setUrl("http://example.com");
+        assertEquals("example.com", webUrl.getDomain());
+        assertEquals("", webUrl.getSubDomain());
+
+        setUrl("http://test.example.com");
+        assertEquals("example.com", webUrl.getDomain());
+        assertEquals("test", webUrl.getSubDomain());
+
+        setUrl("http://test2.test.example.com");
+        assertEquals("example.com", webUrl.getDomain());
+        assertEquals("test2.test", webUrl.getSubDomain());
+
+        setUrl("http://test3.test2.test.example.com");
+        assertEquals("example.com", webUrl.getDomain());
+        assertEquals("test3.test2.test", webUrl.getSubDomain());
+
+        setUrl("http://www.example.ac.jp");
+        assertEquals("example.ac.jp", webUrl.getDomain());
+        assertEquals("www", webUrl.getSubDomain());
+
+        setUrl("http://example.ac.jp");
+        assertEquals("example.ac.jp", webUrl.getDomain());
+        assertEquals("", webUrl.getSubDomain());
+
+        setUrl("http://www.ravn.co.uk");
+        assertEquals("ravn.co.uk", webUrl.getDomain());
+        assertEquals("www", webUrl.getSubDomain());
+    }
+
+    public void testGuavaDomain() {
+        assertEquals("ravn.co.uk", InternetDomainName.from("www.ravn.co.uk").topPrivateDomain().name());
+        assertEquals("ravn.co.uk", InternetDomainName.from("mail.internal.ravn.co.uk").topPrivateDomain().name());
+        assertEquals("microsoft.com", InternetDomainName.from("www.microsoft.com").topPrivateDomain().name());
+        assertEquals("ravn.be", InternetDomainName.from("www.ravn.be").topPrivateDomain().name());
+    }
+
+    public void testGuavaSubDomain() {
+        System.out.println(InternetDomainName.from("www.ravn.co.uk").parent());
+        assertEquals("ravn.co.uk", InternetDomainName.from("www.ravn.co.uk").topPrivateDomain().name());
+        assertEquals("ravn.co.uk", InternetDomainName.from("mail.internal.ravn.co.uk").topPrivateDomain().name());
+        assertEquals("microsoft.com", InternetDomainName.from("www.microsoft.com").topPrivateDomain().name());
+        assertEquals("ravn.be", InternetDomainName.from("www.ravn.be").topPrivateDomain().name());
+    }
+
 }
