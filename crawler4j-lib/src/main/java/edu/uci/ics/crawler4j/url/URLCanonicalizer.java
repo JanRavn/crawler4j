88c983b1629f1b47d81ae4f645a19d0dd9d376cd
Tom Clabon <tom@ravn.co.uk>
2015-03-18 15:03:05 +0000

    Fixed bug in URLCanoncalizer where paths were decoded before the request is made

--- crawler4j-lib/src/main/java/edu/uci/ics/crawler4j/url/URLCanonicalizer.java
+++ crawler4j-lib/src/main/java/edu/uci/ics/crawler4j/url/URLCanonicalizer.java
@@ -17,7 +17,6 @@
 
 package edu.uci.ics.crawler4j.url;
 
-import java.io.UnsupportedEncodingException;
 import java.net.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -197,90 +196,7 @@ public class URLCanonicalizer {
         }
     }
 
-    /**
-     * Percent-dencode values according the RFC 3986.
-     *
-     * @param string Decoded string.
-     * @return Encoded string per RFC 3986.
-     */
-    private static String percentDencodeRfc3986(String string) {
-        try {
-            return decode(string, "UTF-8");
-        } catch (Exception e) {
-            return string;
-        }
-    }
-
-    public static String decode(String s, String enc)
-            throws UnsupportedEncodingException {
-
-        boolean needToChange = false;
-        int numChars = s.length();
-        StringBuffer sb = new StringBuffer(numChars > 500 ? numChars / 2 : numChars);
-        int i = 0;
-
-        if (enc.length() == 0) {
-            throw new UnsupportedEncodingException ("URLDecoder: empty string enc parameter");
-        }
-
-        char c;
-        byte[] bytes = null;
-        while (i < numChars) {
-            c = s.charAt(i);
-            switch (c) {
-                case '%':
-		/*
-		 * Starting with this instance of %, process all
-		 * consecutive substrings of the form %xy. Each
-		 * substring %xy will yield a byte. Convert all
-		 * consecutive  bytes obtained this way to whatever
-		 * character(s) they represent in the provided
-		 * encoding.
-		 */
-
-                    try {
-
-                        // (numChars-i)/3 is an upper bound for the number
-                        // of remaining bytes
-                        if (bytes == null)
-                            bytes = new byte[(numChars-i)/3];
-                        int pos = 0;
-
-                        while ( ((i+2) < numChars) &&
-                                (c=='%')) {
-                            bytes[pos++] =
-                                    (byte)Integer.parseInt(s.substring(i+1,i+3),16);
-                            i+= 3;
-                            if (i < numChars)
-                                c = s.charAt(i);
-                        }
-
-                        // A trailing, incomplete byte encoding such as
-                        // "%x" will cause an exception to be thrown
-
-                        if ((i < numChars) && (c=='%'))
-                            throw new IllegalArgumentException(
-                                    "URLDecoder: Incomplete trailing escape (%) pattern");
-
-                        sb.append(new String(bytes, 0, pos, enc));
-                    } catch (NumberFormatException e) {
-                        throw new IllegalArgumentException(
-                                "URLDecoder: Illegal hex characters in escape (%) pattern - "
-                                        + e.getMessage());
-                    }
-                    needToChange = true;
-                    break;
-                default:
-                    sb.append(c);
-                    i++;
-                    break;
-            }
-        }
-
-        return (needToChange? sb.toString() : s);
-    }
-
     private static String normalizePath(final String path) {
-        return percentDencodeRfc3986(path);
+        return path.replace("%7E", "~").replace(" ", "%20");
     }
 }
